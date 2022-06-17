e7cac2cd9b2714dafb1e922e5281e04239fb9cfb
HieN <sanblas.cerumen@gmail.com>
2022-06-17 19:56:02 +0200

    merch

--- Front/src/views/PokeMerch.vue
+++ Front/src/views/PokeMerch.vue
@@ -1,44 +1,52 @@
 <template>
-  <div v-for="(data, index) in prod_data" :key="index">
-    <h1>{{ data.product_name }}</h1>
-    <h2>{{ data.type }}</h2>
-    <h3>{{ data.price }}</h3>
-    <img :src="require(`${data.url}`)" />
+  <img src="@/assets/pokemerch.png" alt="pokemerch" />
+  <div class="container">
+    <div class="product-card" v-for="data in prod_data" :key="data._id">
+      <h3>{{ data.product_name }}</h3>
+      <h4>{{ data.type }}</h4>
+      <img class="product-image" :src="data.url" alt="data.name" />
+      <h5>{{ data.price }}</h5>
+    </div>
   </div>
 </template>
+
 <script>
 import axios from "axios";
 export default {
   data() {
     return {
-      prod_data: {
-        url: "",
-        product_name: "",
-        type: "",
-        price: "",
-      },
+      prod_data: [],
     };
   },
-  created() {
+  mounted() {
     this.showMerch();
-    console.log(this.prod_data);
   },
   methods: {
     async showMerch() {
-      await axios
-        .get("http://localhost:3000/api/buscar")
-        .then((response) => {
-          this.prod_data.product_name = response.data.product_name;
-          this.prod_data.type = response.data.type;
-          this.prod_data.price = response.data.price;
-          this.prod_data.url = response.data.url;
-        })
-        .catch((e) => {
-          console.log("error " + e);
-        });
+      const response = await axios.get("http://localhost:3000/api/buscar");
+
+      this.prod_data = response.data;
     },
   },
 };
 </script>
-<style lang="">
+<style scoped>
+.container {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(18rem, 1fr));
+  gap: 1em;
+  margin: 40px;
+}
+.product-image {
+  width: 200px;
+}
+.product-card {
+  background: white;
+
+  border-radius: 20px;
+  width: 300px;
+  padding: 30px;
+  -webkit-box-shadow: 5px 5px 15px -3px rgba(0, 0, 0, 0.51);
+  box-shadow: 5px 5px 15px -3px rgba(0, 0, 0, 0.51);
+}
 </style>
